ddd3709ee1461bc589703dbb1d41b383bc2cfe2e
Bryan Mitchell <bryan.bmf@gmail.com>
2024-05-06 15:21:00 -0400

    adding dummy data

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -4,7 +4,6 @@ import {
 	Card,
 	CardBody,
 	CardFooter,
-	Center,
 	Divider,
 	Heading,
 	Image,
@@ -12,31 +11,27 @@ import {
 	Text,
 } from "@chakra-ui/react";
 
-import data from "../data";
 
-const RecipeCard = () => {
-	const recipes: any = data;
-	console.log(recipes);
+const RecipeCard = (props: any) => {
+    const {title, image, cuisine, time, tags} = props.data; 
 
 	return (
-		<Card maxW="xs">
+		<Card maxW="250px" maxH="lg">
 			<CardBody>
-				<Image src={recipes.image} borderRadius="lg" />
+				<Image src={image} borderRadius="lg" boxSize="200px" />
 				<Stack mt="6" spacing="3">
-					<Heading size="md">{recipes.title}</Heading>
-					<Text>Cuisine: {recipes.cuisine}</Text>
-					<Text>Time: {recipes.time}</Text>
-					<Center>
-						<Stack direction="row" spacing="3">
-							{recipes.tags.map((tag:any) => (
-								<Badge colorScheme="orange">{tag}</Badge>
-							))}
-						</Stack>
-					</Center>
+					<Heading size="md">{title}</Heading>
+					<Text>Cuisine: {cuisine}</Text>
+					<Text>Time: {time}</Text>
+					<Stack direction="row" spacing="3" justify="center">
+						{tags.map((tag: string) => (
+							<Badge colorScheme="orange">{tag}</Badge>
+						))}
+					</Stack>
 				</Stack>
 			</CardBody>
 			<Divider />
-			<CardFooter>
+			<CardFooter justify="center">
 				<Button variant="solid" colorScheme="blue">
 					View recipe
 				</Button>
--- src/components/Recipes.tsx
+++ src/components/Recipes.tsx
@@ -1,16 +1,15 @@
 import { SimpleGrid } from "@chakra-ui/react";
+import data from "../data";
 import RecipeCard from "./RecipeCard";
 
 const Recipes = () => {
-	return (
-		<SimpleGrid
-			spacing={4}
-			minChildWidth='200px'
-		>
-			<RecipeCard />
-			<RecipeCard />
-			<RecipeCard />
+	const recipes: any = data;
 
+	return (
+		<SimpleGrid spacing={4} minChildWidth="200px">
+			{recipes.map((recipe: any) => (
+				<RecipeCard data={recipe} />
+			))}
 		</SimpleGrid>
 	);
 };
