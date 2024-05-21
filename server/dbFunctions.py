bc9d9fc3251b4fcac596da27e0d320faeaa81f00
Bryan Mitchell <bryan.bmf@gmail.com>
2024-05-21 13:37:11 -0400

    organize backend functions

---
+++ server/dbFunctions.py
@@ -0,0 +1,108 @@
+from os import environ
+
+import psycopg2
+from flask import Response
+from psycopg2.extras import Json
+
+PG_USER=environ.get('PG_USER')
+PG_PASSWORD=environ.get('PG_PASSWORD')
+PG_DB=environ.get('PG_DB')
+PG_HOST=environ.get('PG_HOST')
+
+
+# db config
+def db_connect():
+    conn = psycopg2.connect(host=PG_HOST,
+                            database=PG_DB,
+                            user=PG_USER,
+                            password=PG_PASSWORD)
+    return conn
+
+def create_update_recipe(req_data, type):
+    print(type + ' recipe')
+    res = 'ok'
+    try:
+        conn = db_connect()
+        cur = conn.cursor()
+        print("Connected to PostgresDB")
+        
+        cur.execute("CALL clean_plate_club.recipe_" + type + "(%s, %s, %s, %s, %s, %s, %s);", (
+            req_data['id'],
+            req_data['name'],
+            req_data['cuisine'],
+            req_data['time'],
+            req_data['protein'],
+            req_data['cooking_type'],
+            Json(req_data['source'])    
+        )) 
+        conn.commit()
+        print('Stored Procedure called')
+    
+    except (Exception, psycopg2.DatabaseError) as error: 
+        res = error
+        print("Error on calling stored procedure", error) 
+    finally:
+        cur.close()
+        conn.close()
+        print("PostgreSQL connection is closed")
+        
+        # send error code when there's an exception
+        if(res != 'ok'):
+            response = Response()
+            response.status_code = 503
+            return response
+            
+        res = 'Recipe ' + type + "d"
+        return res
+    
+def get_edit_recipe(id):
+    print('get/edit recipe')
+    
+    condition = ''
+    if(id != 'all'):
+        condition = "where id = '" + id + "'"
+        
+    query = """ 
+            select jsonb_agg(t)
+	        from (
+                select id, name, cuisine, time, protein, cooking_type, media, image, null as title, null as author, null as cover_image, null as page
+                from clean_plate_club.recipes t1
+                join clean_plate_club.media t2
+                on t1.id = t2.recipe_id
+                and t2.recipe_id is not null
+                """ + condition + """
+
+                union
+
+                select id, name, cuisine, time, protein, cooking_type, null as media, null as image, title, author, cover_image, page
+                from clean_plate_club.recipes t1
+                join clean_plate_club.books t2
+                on t1.id = t2.recipe_id
+                and t2.recipe_id is not null
+                """ + condition + """
+                ) t
+            """
+    try:
+        conn = db_connect()
+        cur = conn.cursor()
+        cur.execute(query)
+        recipes = cur.fetchall()
+        print(recipes)
+        res = recipes
+        
+    except (Exception, psycopg2.DatabaseError) as error: 
+        res = 'error'
+        print(error) 
+        
+    finally:
+        cur.close()
+        conn.close()
+        print("PostgreSQL connection is closed")
+        
+        # send error code when there's an exception
+        if(res == 'error'):
+            response = Response()
+            response.status_code = 503
+            return response
+            
+        return res
